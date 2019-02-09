c3e847a6f57125bc69476600142e0148631b6a85
Chris McClure <chrismcclure2010@gmail.com>
2019-02-09 18:40:59 +0300

    all functions working

--- HW2/pa2.lua
+++ HW2/pa2.lua
@@ -4,14 +4,15 @@
 -- Last revision date: 2/7/19
 -- Purpose: functions being tested for hw2
 
-
 local pa2 = {}
+
+
+-- ******* FUNCTION MAPTABLE *******
 -- mapTable(f, t) takes a function and a table. 
 -- It calls the function passed, adds 10 to values in the table, and
 -- returns the table. 
 -- Ex. tbl = {[abc]=4, [true]=-14, [5] = 7} returns
 -- tbl{ ["abc"]=14, [true]=-4, [5]=17 } 
-
 function pa2.mapTable(f, t)
 	for k, v in pairs(t) do
 		t[k] = f(v)
@@ -24,11 +25,11 @@ local function addTen(n)
 end
 
 
+-- ******* FUNCTION CONCATMAX *******
 -- concatMax(m, n) returns the passed string concatenated with itself
 -- up to the passed number. (Ex. concatMax("abc", 7) returns "abcabc").
 -- If the length of the string is greater than the number passed, the 
 -- function returns the empty string.
-
 function pa2.concatMax(m, n)
 	if (#m > n) then   -- if the length of the string > number, 
 		return "" -- then return the empty string.
@@ -42,6 +43,7 @@ function pa2.concatMax(m, n)
 end
 
 
+-- ******* FUNCTION COLLATZ *******
 -- collatz takes a number k and returns an iterator.
 --  If k is equal to 1, then the function returns 
 --and we're done. If k is odd, then collatz is called 
@@ -69,61 +71,31 @@ function pa2.collatz(k)
 	return iter, nil, nil
 end
 
-function pa2.backSubs(s)
-	local count = 0
-	local z = 0
-	local n =string.len(s)
-	local str_reverse = string.reverse(s)
-	print("REVERSED: " .. str_reverse)
-	local num_substr = (n*(n+1)/2)+1
 
-	while count < num_substr do
-		print("COUNT IN LOOP: " .. count)
-		-- coroutine.yield(string.sub(str_reverse, count, count))
-		coroutine.yield(string.sub(str_reverse, count, count))
+-- ******* COROUTINE BACKSUBS *******
+-- coroutine backsSubs takes in a string "s" and yields all substrings
+-- of the reverse of "s", including the empty string.
+-- ex.) s = "abc", backSubs yields {"", "c", "b", "a", "cb", "ba", "cba"}
+function pa2.backSubs(s)
+	local count = 0 -- loop counter 
+	local start = 0 -- start of string
+	local dist = 0 -- indexed character
+	local n =string.len(s) -- length of string
+	local str_reverse = string.reverse(s) 
+	local num_substr = (n*(n+1)/2)+1 -- the number of possible substrings, including empty string
+
+	for sub_counter = 1, num_substr do
+		coroutine.yield(string.sub(str_reverse, start, count))
 		count = count + 1
+		start = start + 1
+		if count == n+1 then
+			dist = dist + 1 -- distance betweeen bounds
+			start = 1 -- go back to the first non empty character in the string
+			count = start + dist -- far bound = near bound + distance between bounds
+		end
 	end
 end
 
-
-
-return pa2
--- -- ******* TESTING FUNCTIONS ******
-
--- -- ******* function concatMax(m, n) *******
--- io.write("******* FUCNTION MAPTABLE *******\n")
--- io.write("Testing concatMax with 13: " .. concatMax("abc", 13).."\n")
--- io.write("Testing concatMax with 20: " .. concatMax("aardvark", 20).."\n")
--- io.write("Testing concatMax with 10: " .. concatMax("aardvark", 10).."\n")
--- io.write("Testing concatMax with 5: " .. concatMax("aardvark", 5).."\n")
-
--- io.write("\n")
-
--- -- ******* function mapTable(f, t) *******
--- io.write("******* FUCNTION MAPTABLE *******\n")
--- tbl = {[2]=1, ["abc"]=20, [true] = -10, ["xyz"] = -127, [false] = 14,}
--- -- io.write(mapTable(addTen, tbl))
--- tbl1 = mapTable(tbl, addTen)
--- for k, v in pairs(tbl1) do
--- 	print("v: " .. v)
--- end
--- io.write("\n")
--- -- ******* function collatz(k) *******
--- io.write("******* FUCNTION COLLATZ *******\n")
--- io.write("Testing collatz with 9: \n")
--- collatz(9)
--- io.write("\n")
--- io.write("Testing collatz with 3: \n")
--- collatz(3)
--- io.write("\n")
--- io.write("Testing collatz with 1: \n")
--- collatz(1)
-
--- io.write("\n")
-
--- -- ******* coroutine backsubs *******
--- io.write("******* COROUTINE BACKSUBS *******\n")
-
--- io.write("\n")
+return pa2 -- return module to caller
 
 
