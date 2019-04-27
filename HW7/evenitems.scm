456bba8fc003a693f33c9482d2c9acb507432c73
Chris McClure <chrismcclure2010@gmail.com>
2019-04-27 23:10:16 +0300

    function working correctly, done

--- HW7/evenitems.scm
+++ HW7/evenitems.scm
@@ -1,15 +1,28 @@
 #lang scheme
+; Chris McClure
+; evenitems.scm
+; 27 Apr 2019
 
-(define (thing lst)
+; Returns a list of values that
+; occupied even indices in a list that was passed.
+
+; evenitems
+; calls helper function assist
+(define (evenitems lst)
   (assist lst 0)
   )
 
+; assist
+; takes a list and a number
+; if the list is empty, it returns the empty list
+; else if the number is even, then create a list and append the first
+; element to the front of it, then increment the num and call assist again
+; otherwise, the number is odd. only increment the number and call assist again.
 (define (assist lst num)
   (cond
-    [(empty? lst) 0]
+    [(empty? lst) null]
     [(even? num)
-     (list (first lst) (assist (rest lst) (+ 1 num)))
-     ]
+     (append (list (first lst)) (assist (rest lst) (+ 1 num)))]
     [else (assist (rest lst) (+ 1 num))]
     )
   )
